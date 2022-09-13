51c85368be3fd577ec5e9baa21b9d095e02cfd19
dfowler5113 <dfowler5113@gmail.com>
2022-09-13 07:55:35 -0500

    First commit

---
+++ src/TicTacToe/TicTacToe.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const TicTacToe = () => {
+    const Cell = () =>{
+        return <td>-</td>
+    }
+  return (
+    <div>
+        <table>
+            <tbody>
+                 <tr>
+                     <Cell/>
+                     <Cell/>
+                     <Cell/>
+                </tr>
+                <tr>
+                     <Cell/>
+                     <Cell/>
+                     <Cell/>
+                </tr>
+                <tr>
+                     <Cell/>
+                     <Cell/>
+                     <Cell/>
+                </tr>
+            </tbody>
+        </table> 
+    </div>
+  );
+};
+
+export default TicTacToe
